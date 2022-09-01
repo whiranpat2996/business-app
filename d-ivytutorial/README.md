efd829ef3c02e2d6762041419aa7107cc6535277
whiranpat2996 <112653699+whiranpat2996@users.noreply.github.com>
2022-09-01 14:33:23 -0400

    Initial upload

---
+++ d-ivytutorial/README.md
@@ -0,0 +1,4 @@
+# business-app
+# business-app
+# business-app
+# business-app
---
+++ d-ivytutorial/crm1/accounts/urls.py
@@ -0,0 +1,9 @@
+from django.http import HttpResponse
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name="home"),
+    path('products/', views.products, name="products"),
+    path('customer/<str:pk_test>/', views.customer, name="customer"),
+]
